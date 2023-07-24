888369f4f1dd885419fcf49953843d196071990f
hoangnhan <nhan.nguyen@phoenix-software.net>
2023-07-24 14:49:09 +0700

    spring created

---
+++ src/main/java/net/phoenixsoftware/PhoenixProject/PhoenixProjectApplication.java
@@ -0,0 +1,13 @@
+package net.phoenixsoftware.PhoenixProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/phoenixsoftware/PhoenixProject/PhoenixProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.phoenixsoftware.PhoenixProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
