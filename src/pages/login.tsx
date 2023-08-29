156ccef61d770aa1ca54d32fa20e3dd41b762640
hoangnhan <nhan.nguyen@phoenix-software.net>
2023-08-29 17:58:31 +0700

    Add API call to login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,6 +5,9 @@ import NextHead from 'next/head'
 import Image from 'next/image'
 import { LoadingButton } from '@mui/lab';
 import Link from "next/link";
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
+
 
 // import logo from '../public/hyster-yale-logo.gif'
 
@@ -21,7 +24,50 @@ const StyledFormContainer = styled(Paper)(({ theme }) => ({
     padding: theme.spacing(3)
   }))
 
+const queryString = require('querystring-es3');
+
 export default function LoginPage() {
+
+  const [cookies, setCookie, removeCookie] = useCookies(['accessToken']);
+
+  const getAccessToken = () => {
+    // End destination for Authorization
+    const BASE_URL = "http://192.168.1.154:8080";
+    const endUrl = `${BASE_URL  }/oauth/token`;
+    let accessToken;
+
+    // Configure body for Authorization and get Access Token
+    const data = queryString.stringify({
+        "grant_type": "password",
+        "username": document.getElementById('inputEmail').value,
+        "password": document.getElementById('inputPassword').value
+    })
+
+
+    // Configure headers and Basic Authentication
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',  
+        },
+        auth: {
+            username: 'client',
+            password: 'password'
+        }
+    }
+    
+    axios.post(endUrl, data, options)
+    .then(response => {
+        console.log(response);
+        accessToken = response.data.access_token;
+        setCookie('accessToken', accessToken);
+        setCookie('authInformation', response.data)
+    }).catch(error => {
+        console.log("Something went wrong");
+        console.log(error);
+    });
+  }
+
   return (
     <>
       <NextHead>
@@ -34,11 +80,13 @@ export default function LoginPage() {
             Hyster-Yale{/* <Image src={logo as any} alt="The logo" width={160} height={40} /> */}
         </div>
         <AppTextField
+          id='inputEmail'
           sx={{ mt: 2}}
-          label="User Name"
+          label="Email"
           name="drawing_id"
         />
         <AppTextField
+          id='inputPassword'
           sx={{ mt: 2 }}
           label="Password"
           name="drawing_id"
@@ -56,6 +104,7 @@ export default function LoginPage() {
             fullWidth
             variant="contained"
             color="primary"
+            onClick={getAccessToken}
             // loading={loading}
             // disabled={loadingSSO || loading}
           >
