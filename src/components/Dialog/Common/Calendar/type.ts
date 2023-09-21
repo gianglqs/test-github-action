609951dd7b853ba9b452d433e75d9ba0d6a88046
Trong Luu <trong.luu@edge-works.net>
2023-09-21 15:28:40 +0700

    chore: set type for dialog calendar

--- src/components/Dialog/Common/Calendar/type.ts
+++ src/components/Dialog/Common/Calendar/type.ts
@@ -1,14 +1,6 @@
-/*******************************************************
-Copyright (C) - DREVER International
+import { AppDialogProps } from "../../Module/AppDialog/type"
 
-This file is part of Malis 3 project.
-
-Malis 3 source code can not be copied and/or distributed without the express permission of DREVER International
-*******************************************************/
-
-import type { AppDialogProps } from '@/components/Dialog/AppDialog/type'
-
-export interface DialogCalendarProps extends Omit<AppDialogProps, 'onChange'> {
+export interface DialogCalendarProps extends Omit<AppDialogProps, "onChange"> {
   date: string
   disabled?: boolean
   onChange: (event, stringDate: string, date: Date) => void
