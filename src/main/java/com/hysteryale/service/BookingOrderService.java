15b30b8d377310233e3a5f5544f8636832ffa984
gianglqs <son.giang@phoenix-software.net>
2023-10-02 10:17:47 +0700

    import bookingOrderPart successfully

--- src/main/java/com/hysteryale/service/BookingOrderService.java
+++ src/main/java/com/hysteryale/service/BookingOrderService.java
@@ -120,10 +120,10 @@ public class BookingOrderService {
                                 field.set(bookingOrder, cell.getStringCellValue());
                                 break;
                             case "int":
-                                if (cell.getCellType() == (CellType.STRING.getCode())) {
+                                if (cell.getCellType() == (CellType.STRING)) {
                                     log.info("Cell column " + cell.getColumnIndex() + " row " + cell.getRowIndex() + " " + cell.getStringCellValue());
                                     field.set(bookingOrder, Integer.parseInt(cell.getStringCellValue()));
-                                } else if (cell.getCellType() == (CellType.NUMERIC.getCode())) {
+                                } else if (cell.getCellType() == (CellType.NUMERIC)) {
                                     log.info("Cell column " + cell.getColumnIndex() + " row " + cell.getRowIndex() + " " + cell.getStringCellValue());
                                     field.set(bookingOrder, (int) cell.getNumericCellValue());
                                 }
