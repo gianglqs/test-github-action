db5542bc2a0beeffc499c9b993f60405ad550c6f
hoangnhan <nhan.nguyen@phoenix-software.net>
2023-10-13 10:42:32 +0700

    Implement MarginAnalystFileUpload

---
+++ src/main/java/com/hysteryale/repository/marginAnalyst/MarginAnalystFileUploadRepository.java
@@ -0,0 +1,7 @@
+package com.hysteryale.repository.marginAnalyst;
+
+import com.hysteryale.model.marginAnalyst.MarginAnalystFileUpload;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarginAnalystFileUploadRepository extends JpaRepository<MarginAnalystFileUpload, Integer> {
+}
