afc73a421f356a7adb6de43bafe713bb5796fbe0
hoangnhan <nhan.nguyen@phoenix-software.net>
2023-10-17 17:06:35 +0700

    delete unused model

--- src/main/java/com/hysteryale/repository/marginAnalyst/inmemory/IMMarginAnalystDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hysteryale.repository.marginAnalyst.inmemory;
-
-import com.hysteryale.model.marginAnalyst.inmemory.IMMarginAnalystData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IMMarginAnalystDataRepository extends JpaRepository<IMMarginAnalystData, Integer> {
-}
