fc1712b509944ed7b0bbfcd8256437441d6f4d5c
gianglqs <son.giang@phoenix-software.net>
2023-10-30 11:58:41 +0700

    update import new data BookingOrder

--- src/main/java/com/hysteryale/controller/ImportController.java
+++ src/main/java/com/hysteryale/controller/ImportController.java
@@ -44,9 +44,9 @@ public class ImportController {
 //        apicDealerService.importAPICDealer();
 //        apacSerialService.importAPACSerial();
 //        currencyService.importCurrencies(curencyFolder);
-//        partService.importPart();
-  //      aopMarginService.importAOPMargin();
-    //    productDimensionService.importProductDimension();
+        partService.importPart();
+        aopMarginService.importAOPMargin();
+        productDimensionService.importProductDimension();
         bookingOrderService.importOrder();
 //        exchangeRateService.importExchangeRate();
 //        costUpliftService.importCostUplift();
