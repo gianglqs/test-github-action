04b30ff9a954f1269eae97063f7d16fe9f1feed6
gianglqs <son.giang@phoenix-software.net>
2023-11-13 11:09:27 +0700

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -69,7 +69,7 @@ export default function LoginPage() {
       axios
          .post(
             // 'http://192.168.1.150:8080/hysteryale/oauth/token',
-            'http://localhost:8080/oauth/token',
+            'http://localhost:8081/oauth/token',
             transformData,
             options
          )
