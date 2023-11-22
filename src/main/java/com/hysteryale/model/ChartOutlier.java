c1864a1139de3252eb455d26d363d19bccd16b12
hoangnhan <nhan.nguyen@phoenix-software.net>
2023-11-22 18:13:26 +0700

    implement get data for scatter chart

---
+++ src/main/java/com/hysteryale/model/ChartOutlier.java
@@ -0,0 +1,15 @@
+package com.hysteryale.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ChartOutlier {
+    private String region;
+    private double dealerNet;
+    private double marginPercentageAfterSurcharge;
+}
