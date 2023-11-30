d457e2d255ea29dd5763176cf597b7f6a421e904
gianglqs <son.giang@phoenix-software.net>
2023-11-30 19:36:26 +0700

    update

--- src/pages/bookingOrder.tsx
+++ src/pages/bookingOrder.tsx
@@ -1,21 +1,20 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import { formatNumbericColumn } from '@/utils/columnProperties';
 import { formatNumber, formatNumberPercentage, formatDate } from '@/utils/formatCell';
 import { useDispatch, useSelector } from 'react-redux';
 import { bookingStore, commonStore } from '@/store/reducers';
-import { DataGrid } from '@mui/x-data-grid';
+import { useDropzone } from 'react-dropzone';
 
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import { Button } from '@mui/material';
+import { Button, CircularProgress, Typography } from '@mui/material';
 
 import {
    AppAutocomplete,
    AppDateField,
    AppLayout,
    AppTextField,
-   DataTable,
    DataTablePagination,
 } from '@/components';
 
@@ -24,6 +23,12 @@ import { produce } from 'immer';
 
 import { defaultValueFilterOrder } from '@/utils/defaultValues';
 import { DataGridPro, GridToolbar } from '@mui/x-data-grid-pro';
+import axios from 'axios';
+import { parseCookies, setCookie } from 'nookies';
+
+interface FileChoosed {
+   name: string;
+}
 
 export default function Booking() {
    const dispatch = useDispatch();
@@ -31,6 +36,14 @@ export default function Booking() {
    const initDataFilter = useSelector(bookingStore.selectInitDataFilter);
 
    const [dataFilter, setDataFilter] = useState(defaultValueFilterOrder);
+
+   const [loading, setLoading] = useState(false);
+
+   const [uploadedFile, setUploadedFile] = useState<FileChoosed[]>([]);
+
+   const appendFileIntoList = (file) => {
+      setUploadedFile((prevFiles) => [...prevFiles, file]);
+   };
 
    const handleChangeDataFilter = (option, field) => {
       setDataFilter((prev) =>
@@ -198,6 +211,53 @@ export default function Booking() {
       },
    ];
 
+   console.log(' file name: ', uploadedFile);
+
+   const handleUploadFile = async (file) => {
+      let formData = new FormData();
+      formData.append('file', file);
+
+      let cookies = parseCookies();
+      let token = cookies['token'];
+      axios({
+         method: 'post',
+         url: `${process.env.NEXT_PUBLIC_BACKEND_URL}importNewBooking`,
+         data: formData,
+         headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: 'Bearer' + token,
+         },
+      })
+         .then(function (response) {
+            setLoading(false);
+            setCookie(null, 'fileUUID', response.data.fileUUID);
+            handleWhenImportSuccessfully(response);
+         })
+         .catch(function (response) {
+            // show message in screen
+            setLoading(false);
+            //show message
+            dispatch(commonStore.actions.setErrorMessage('Import data error!'));
+         });
+   };
+
+   const handleWhenImportSuccessfully = (res) => {
+      //show message
+      dispatch(commonStore.actions.setSuccessMessage(res.data.message));
+
+      dispatch(bookingStore.sagaGetList());
+   };
+
+   const handleImport = () => {
+      if (uploadedFile.length > 0) {
+         // resert message
+         setLoading(true);
+         handleUploadFile(uploadedFile);
+      } else {
+         dispatch(commonStore.actions.setErrorMessage('No file choosed'));
+      }
+   };
+
    return (
       <>
          <AppLayout entity="booking">
@@ -271,7 +331,6 @@ export default function Booking() {
                      getOptionLabel={(option) => `${option.value}`}
                   />
                </Grid>
-
                <Grid item xs={2}>
                   <AppAutocomplete
                      options={initDataFilter.classes}
@@ -380,6 +439,31 @@ export default function Booking() {
                      Filter
                   </Button>
                </Grid>
+
+               <Grid item xs={1}>
+                  <Button
+                     variant="contained"
+                     onClick={handleImport}
+                     sx={{ width: '100%', height: 24 }}
+                  >
+                     Import
+                  </Button>
+               </Grid>
+
+               <Grid item xs={1}>
+                  <UploadFileDropZone
+                     uploadedFile={uploadedFile}
+                     setUploadedFile={appendFileIntoList}
+                     handleUploadFile={handleUploadFile}
+                     resetUploadFile={setUploadedFile}
+                  />
+               </Grid>
+               <Grid item xs={4}>
+                  {uploadedFile &&
+                     uploadedFile.map((file) => (
+                        <Typography fontSize={13}>File uploaded: {file.name}</Typography>
+                     ))}
+               </Grid>
             </Grid>
 
             <Paper elevation={1} sx={{ marginTop: 2 }}>
@@ -398,6 +482,30 @@ export default function Booking() {
                      columns={columns}
                      getRowId={(params) => params.orderNo}
                   />
+                  {loading ? (
+                     <div
+                        style={{
+                           top: 0,
+                           left: 0,
+                           right: 0,
+                           bottom: 0,
+                           backgroundColor: 'rgba(0,0,0, 0.3)',
+                           position: 'absolute',
+                           display: 'flex',
+                           justifyContent: 'center',
+                           alignItems: 'center',
+                           zIndex: 1001,
+                        }}
+                     >
+                        <CircularProgress
+                           color="info"
+                           size={60}
+                           sx={{
+                              position: 'relative',
+                           }}
+                        />
+                     </div>
+                  ) : null}
                </Grid>
                <DataTablePagination
                   page={tableState.pageNo}
@@ -409,5 +517,59 @@ export default function Booking() {
             </Paper>
          </AppLayout>
       </>
+   );
+}
+
+function UploadFileDropZone(props) {
+   const onDrop = useCallback((acceptedFiles) => {
+      props.resetUploadFile([]);
+      acceptedFiles.forEach((file) => {
+         const reader = new FileReader();
+
+         reader.onabort = () => console.log('file reading was aborted');
+         reader.onerror = () => console.log('file reading has failed');
+         reader.onload = () => {
+            // Do whatever you want with the file contents
+            //  const binaryStr = reader.result;
+            //console.log('binaryStr', binaryStr);
+            props.setUploadedFile(file);
+         };
+         reader.readAsArrayBuffer(file);
+      });
+   }, []);
+
+   const { getRootProps, getInputProps, open, fileRejections } = useDropzone({
+      noClick: true,
+      onDrop,
+      maxSize: 10485760, // < 10MB
+      maxFiles: 3,
+      accept: {
+         'excel/xlsx': ['.xlsx'],
+      },
+   });
+   const dispatch = useDispatch();
+   const isFileInvalid = fileRejections.length > 0 ? true : false;
+   if (isFileInvalid) {
+      const errors = fileRejections[0].errors;
+      dispatch(
+         commonStore.actions.setErrorMessage(
+            `${errors[0].message} ${_.isNil(errors[1]) ? '' : `or ${errors[1].message}`}`
+         )
+      );
+      fileRejections.splice(0, 1);
+   }
+
+   return (
+      <div {...getRootProps()}>
+         <input {...getInputProps()} />
+         <Button
+            type="button"
+            onClick={open}
+            variant="contained"
+            sx={{ width: '100%', height: 24 }}
+         >
+            Select file
+         </Button>
+      </div>
    );
 }
