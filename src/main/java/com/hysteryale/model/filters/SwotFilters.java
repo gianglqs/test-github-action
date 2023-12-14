4ddf33889a495a79d4693b5609db0ff3e0380563
hoangnhan <nhan.nguyen@phoenix-software.net>
2023-12-14 10:50:38 +0700

    add filter model for Swot Bubble Chart

---
+++ src/main/java/com/hysteryale/model/filters/SwotFilters.java
@@ -0,0 +1,20 @@
+package com.hysteryale.model.filters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SwotFilters {
+    private String regions;
+    private List<String> countries;
+    private String classes;
+    private String categories;
+    private List<String> series;
+}
