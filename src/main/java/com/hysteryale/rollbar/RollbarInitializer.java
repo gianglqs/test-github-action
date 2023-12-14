b484652fe320bfb19b3cd9da461177b2738fdbc4
gianglqs <son.giang@phoenix-software.net>
2023-12-14 18:55:58 +0700

    disable rollbar

--- src/main/java/com/hysteryale/rollbar/RollbarInitializer.java
+++ src/main/java/com/hysteryale/rollbar/RollbarInitializer.java
@@ -1,35 +1,35 @@
 package com.hysteryale.rollbar;
 
-import com.rollbar.notifier.Rollbar;
+//import com.rollbar.notifier.Rollbar;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
-import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
+//import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
 
 @Configuration
 public class RollbarInitializer {
 
-    protected static Rollbar rollbar;
-
-    @Value("${rollbar.secret-key}")
-    private String SECRET_KEY;
-
-    @Value("${rollbar.environment}")
-    private String ENV;
-
-    @Value("${rollbar.code-version}")
-    private String CODE_VERSION;
-
-
-    @PostConstruct
-    public void init() {
-        // currently I disabled rollbar because it is running out of monthly quota
-        rollbar = Rollbar.init(withAccessToken(SECRET_KEY)
-                .environment(ENV)
-                .codeVersion(CODE_VERSION)
-                .enabled(false)
-                .build());
-    }
+//    protected static Rollbar rollbar;
+//
+//    @Value("${rollbar.secret-key}")
+//    private String SECRET_KEY;
+//
+//    @Value("${rollbar.environment}")
+//    private String ENV;
+//
+//    @Value("${rollbar.code-version}")
+//    private String CODE_VERSION;
+//
+//
+//    @PostConstruct
+//    public void init() {
+//        // currently I disabled rollbar because it is running out of monthly quota
+//        rollbar = Rollbar.init(withAccessToken(SECRET_KEY)
+//                .environment(ENV)
+//                .codeVersion(CODE_VERSION)
+//                .enabled(false)
+//                .build());
+//    }
 }
