0b4e4b421dd01f2a12d7bb3e0b18c20a56217251
gianglqs <son.giang@phoenix-software.net>
2023-12-19 17:28:15 +0700

    fix bug login page when not found BE

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export async function getServerSideProps(context) {
          },
       };
    } catch (error) {
-      if (error.response.status == 403)
+      if (error.response?.status == 403)
          return {
             redirect: {
                destination: '/bookingOrder',
