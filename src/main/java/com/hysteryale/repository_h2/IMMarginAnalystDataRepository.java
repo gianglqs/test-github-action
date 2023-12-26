cddcb359288cf3aef9d09d1c4dd2da0b43f85ecb
hoangnhan <nhan.nguyen@phoenix-software.net>
2023-12-26 14:28:57 +0700

    modify calculating MarginData by Series

--- src/main/java/com/hysteryale/repository_h2/IMMarginAnalystDataRepository.java
+++ src/main/java/com/hysteryale/repository_h2/IMMarginAnalystDataRepository.java
@@ -8,19 +8,29 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface IMMarginAnalystDataRepository extends JpaRepository<IMMarginAnalystData, Integer> {
-    @Query("SELECT m from IMMarginAnalystData m WHERE m.modelCode = :model_code AND m.currency = :currency AND m.type = :type AND m.fileUUID = :fileuuid")
+    @Query("SELECT m from IMMarginAnalystData m " +
+            "WHERE ((:model_code) IS NULL OR m.modelCode = (:model_code)) " +
+            "AND ((:series) IS NULL OR m.series = (:series)) " +
+            "AND m.currency = :currency " +
+            "AND ((:type) IS NULL OR m.type = (:type)) " +
+            "AND m.fileUUID = :fileuuid " +
+            "ORDER BY m.type, m.modelCode")
     List<IMMarginAnalystData> getIMMarginAnalystData(@Param("model_code") String modelCode, @Param("currency") String strCurrency,
-                                                     @Param("fileuuid") String fileUUID, @Param("type") Integer type);
+                                                     @Param("fileuuid") String fileUUID, @Param("type") Integer type,
+                                                     @Param("series") String series);
 
-    @Query("SELECT m from IMMarginAnalystData m WHERE m.modelCode = :model_code AND m.orderNumber = :order_number AND m.currency = :currency AND m.type = :type AND m.fileUUID = :fileuuid")
+    @Query("SELECT m from IMMarginAnalystData m " +
+            "WHERE ((:model_code) IS NULL OR m.modelCode = (:model_code)) " +
+            "AND ((:series) IS NULL OR m.series = (:series)) " +
+            "AND m.orderNumber = :order_number " +
+            "AND m.currency = :currency " +
+            "AND ((:type) IS NULL OR m.type = (:type)) " +
+            "AND m.fileUUID = :fileuuid")
     List<IMMarginAnalystData> getUSPlantIMMarginAnalystData(@Param("model_code") String modelCode, @Param("order_number") String orderNumber,
                                                             @Param("currency") String currency, @Param("type") Integer type,
-                                                            @Param("fileuuid") String fileUUID);
+                                                            @Param("fileuuid") String fileUUID, @Param("series") String series);
 
-    @Query("SELECT DISTINCT m.modelCode FROM IMMarginAnalystData m WHERE m.fileUUID = ?1")
-    List<String> getModelCodesByFileUUID(String fileUUID);
-
-    @Query("SELECT m FROM IMMarginAnalystData m WHERE m.fileUUID = ?1")
-    List<IMMarginAnalystData> getMargin(String fileUUID);
+    @Query("SELECT DISTINCT m.modelCode FROM IMMarginAnalystData m WHERE m.fileUUID = ?1 and m.series = ?2")
+    List<String> getModelCodesBySeries(String fileUUID, String series);
 
 }
