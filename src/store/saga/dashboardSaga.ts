af515d2af359a5e4250e8935cef10e46084e1a73
hoangnhan <nhan.nguyen@phoenix-software.net>
2024-01-03 16:07:28 +0700

    implement store for user page pagination

--- src/store/saga/dashboardSaga.ts
+++ src/store/saga/dashboardSaga.ts
@@ -1,17 +1,32 @@
-import { takeEvery, put } from "redux-saga/effects"
-import { dashboardStore } from "../reducers"
-import { call } from "typed-redux-saga"
-import dashboardApi from "@/api/dashboard.api"
+import { takeEvery, put } from 'redux-saga/effects';
+import { commonStore, dashboardStore } from '../reducers';
+import { select, call, all } from 'typed-redux-saga';
+import dashboardApi from '@/api/dashboard.api';
 
 function* fetchUserList() {
-  try {
-    const { data } = yield* call(dashboardApi.getUser, { search: "" })
-    yield put(dashboardStore.actions.setUserList(JSON.parse(data)?.userList))
-  } catch (error) {}
+   try {
+      const { tableState } = yield* all({
+         tableState: select(commonStore.selectTableState),
+      });
+
+      const { data } = yield* call(dashboardApi.getUser, {
+         search: '',
+         perPage: tableState.perPage,
+         pageNo: tableState.pageNo,
+      });
+      yield put(dashboardStore.actions.setUserList(JSON.parse(data)?.userList));
+      yield put(dashboardStore.actions.setTotalRow(JSON.parse(data)?.totalItems));
+
+      yield put(
+         commonStore.actions.setTableState({
+            totalItems: JSON.parse(data)?.totalItems,
+         })
+      );
+   } catch (error) {}
 }
 
 function* dashboardSaga() {
-  yield takeEvery(dashboardStore.sagaGetList, fetchUserList)
+   yield takeEvery(dashboardStore.sagaGetList, fetchUserList);
 }
 
-export default dashboardSaga
+export default dashboardSaga;
