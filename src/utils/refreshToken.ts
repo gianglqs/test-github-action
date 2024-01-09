3e9d8b3ac4f6e095272859ded6813ecb0ea4eb69
gianglqs <son.giang@phoenix-software.net>
2024-01-09 18:05:03 +0700

    refreshTokenForFunctionGetServerSidePropsLogin: will be callback by function getServerSideProps  of pages '/login' and '/' when token is expired . it call API to get new access_token, then recall api checkToken ('oauth/checkTokenOfAdmin'). if response have code is 200: Role is ADMIN -> redirect to dashboard if response have code is 403 : Role is USER -> redirect to page Booking else: token is invalid -> login

---
+++ src/utils/refreshToken.ts
@@ -0,0 +1,88 @@
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
+import { GetServerSidePropsContext } from 'next';
+
+export const refreshTokenForFunctionGetServerSideProps = async (
+   context: GetServerSidePropsContext
+) => {
+   try {
+      const cookies = parseCookies(context);
+      const refresh_token = cookies['refresh_token'];
+
+      const response = await axios.post(
+         `${process.env.NEXT_PUBLIC_BACKEND_URL}oauth/refreshToken`,
+         {
+            refreshToken: refresh_token,
+         }
+      );
+
+      const accessToken = response.data.accessToken;
+
+      setCookie(null, 'token', accessToken, {
+         maxAge: 604800,
+         path: '/',
+      });
+      return {
+         props: {},
+      };
+   } catch (error) {
+      return {
+         redirect: {
+            destination: '/login',
+            permanent: false,
+         },
+      };
+   }
+};
+
+export const refreshTokenForFunctionGetServerSidePropsLogin = async (
+   error: AxiosError,
+   context: GetServerSidePropsContext
+) => {
+   try {
+      const cookies = parseCookies(context);
+      const refresh_token = cookies['refresh_token'];
+
+      const response = await axios.post(
+         `${process.env.NEXT_PUBLIC_BACKEND_URL}oauth/refreshToken`,
+         {
+            refreshToken: refresh_token,
+         }
+      );
+
+      const accessToken = response.data.accessToken;
+
+      setCookie(null, 'token', accessToken, {
+         maxAge: 604800,
+         path: '/',
+      });
+      const newRequest = {
+         ...error.config,
+         headers: {
+            ...error.config.headers,
+            Authorization: 'Bearer ' + accessToken,
+         },
+      };
+
+      await axios(newRequest);
+      return {
+         redirect: {
+            destination: '/web-pricing-tools/admin/dashboard',
+            permanent: false,
+         },
+      };
+   } catch (error) {
+      if (error.response?.status == 403) {
+         //role: USER
+         return {
+            redirect: {
+               destination: '/web-pricing-tools/bookingOrder',
+               permanent: false,
+            },
+         };
+      }
+      return {
+         props: {},
+      };
+   }
+};
